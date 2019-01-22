395c941066b35fac457f511c31e2172c50333305
Yolanda Harris <yharkew@Yolandas-MacBook-Pro.local>
2019-01-21 21:59:01 -0500

    pod installs

---
+++ Winstagram/Winstagram/auth/AuthVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Winstagram
+//
+//  Created by Jibril Harris on 1/21/19.
+//  Copyright © 2019 ifaadev. All rights reserved.
+//
+import UIKit
+import FirebaseAuthUI
+
+
+class AuthVC: UIViewController {
+
+    // check if user is authenticated
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    
+    // nill
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // authenticate user
+    @IBAction func authBtn(_ sender: Any) {
+        
+    }
+}
