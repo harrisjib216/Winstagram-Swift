4fd722c50cb5914dc13a8faefebc21c653e64c94
objcmate <objcmate@objcmates-MacBook-Pro.local>
2019-02-25 15:38:40 -0500

    initial camera build

--- Winstagram/create/CreateVC.swift
+++ Winstagram/create/CreateVC.swift
@@ -11,9 +11,37 @@ import AVFoundation
 
 
 class CreateViewController: UIViewController {
-    
+    private let session = AVCaptureSession()
+    var previewView:AVCaptureVideoPreviewLayer!
+    var input:AVCaptureDeviceInput!
+    var device:AVCaptureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        previewView = AVCaptureVideoPreviewLayer()
+        previewView.session = session
+    }
+    
+    
+    // flip camera
+    @IBAction func toggleCam(_ sender: Any) {
+        if (device.position == .back) {
+            self.device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)!
+        }
+        else {
+            self.device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)!
+        }
+    }
+
+    
+    // toggle flash
+    @IBAction func toggleFlash(_ sender: Any) {
+        if (device.position == .back) {
+            self.device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)!
+        }
+        else {
+            self.device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)!
+        }
     }
 }
